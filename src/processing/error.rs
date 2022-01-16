641d3bbf1114873fb94c15db9d2358bb0fb2e471
Matias Fontanini <matias.fontanini@gmail.com>
2022-01-15 16:15:43 -0800

    Use transparent error repr for client errors

--- src/processing/error.rs
+++ src/processing/error.rs
@@ -2,7 +2,7 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("client: {0}")]
+    #[error(transparent)]
     Client(#[from] crate::client::Error),
 
     #[error("unsupported pull request state: {0}")]
